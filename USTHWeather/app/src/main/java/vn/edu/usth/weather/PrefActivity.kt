03e3106d68435f9ddc2167056fcb16e2c5c7ad16
Nguyen Tien Dat <datnt.22bi13079@usth.edu.vn>
2024-09-28 15:19:02 +0700

    lw12

---
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/PrefActivity.kt
@@ -0,0 +1,20 @@
+package vn.edu.usth.weather
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PrefActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_pref)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,11 +1,21 @@
 package vn.edu.usth.weather;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -14,13 +24,19 @@ import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+
 import com.google.android.material.tabs.TabLayout;
+
+
 
 public class WeatherActivity extends AppCompatActivity {
     public static final String TAG = "Weather Activity";
-    private ViewPagerAdapter viewPagerAdapter;
+    public static int action_setting = 1000005;
+    public static int action_refresh = 100000;
     private ViewPager viewPager;
     private TabLayout tabLayout;
+    private Toolbar toolbar;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,12 +50,64 @@ public class WeatherActivity extends AppCompatActivity {
         });
 
         ViewPager viewPager = findViewById(R.id.viewPager);
-        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(viewPagerAdapter);
 
         TabLayout tabLayout = findViewById(R.id.tabLayout);
         tabLayout.setupWithViewPager(viewPager);
 
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.music1);
+        mediaPlayer.start();
+
+
+
+    }
+
+    public boolean  onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == action_setting) {
+            Toast.makeText(this, "Setting", Toast.LENGTH_SHORT).show();
+            final Handler handler = new Handler(Looper.getMainLooper()) {
+                @Override
+                public void handleMessage(Message msg) {
+// This method is executed in main thread
+                    String content = msg.getData().getString("server_response");
+                    Toast.makeText(WeatherActivity.this, content, Toast.LENGTH_SHORT).show();
+                }
+            };
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+// this method is run in a worker thread
+                    try {
+// wait for 5 seconds to simulate a long network access
+                        Thread.sleep(5000);
+                    }
+                    catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+// Assume that we got our data from server
+                    Bundle bundle = new Bundle();
+                    bundle.putString("server_response", "some sample json here");
+// notify main thread
+                    Message msg = new Message();
+                    msg.setData(bundle);
+                    handler.sendMessage(msg);
+                }
+            });
+            t.start();
+            return true;
+        } else if (item.getItemId() == action_refresh) {
+            Intent intent = new Intent(this, PrefActivity.class);
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
     }
     protected void onPause(){
         super.onPause();
